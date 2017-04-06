2eba3edcd6ced915ee60f5b0b822c1294719a0f1
PAlvarezGarcia <uo245548@uniovi.es>
2017-04-06 10:10:43 +0200

    Codificación para acentos y eliminacion mvnw del proyecto

--- src/test/java/hello/MainControllerTest.java
+++ src/test/java/hello/MainControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -23,7 +24,7 @@ import pSystem.UserInfo;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-@IntegrationTest({"server.port=0"})
+@IntegrationTest({"server.port=8120"})
 public class MainControllerTest {
 
     @Value("${local.server.port}")
@@ -40,9 +41,10 @@ public class MainControllerTest {
 
 	@Test
 	public void getLanding() throws Exception {
-		String userURI = base.toString() + "/user";  
+		//String userURI = base.toString() + "/user";  
 		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
-		assertThat(response.getBody(), containsString("Hola"));
+		assertThat(response.getBody(), containsString("Usuario:"));
+		
 	}
 	
 	@Test
